524b2d24bb120621c8b3f685bb7abbcf3a33e485
Flittly <2605948957@qq.com>
2026-09-09 21:20:22 +0800

    chore(version): 更新项目版本号至 1.2.0
    - 将 admin 项目的版本从 1.1.0 更新到 1.2.0
    - 将 desktop 项目的版本从 1.1.0 更新到 1.2.0
    - 更新 preload.js 中的默认版本号为 1.2.0

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("eduplayDesktop", {
-  version: process.env.npm_package_version ?? "1.1.0",
+  version: process.env.npm_package_version ?? "1.2.0",
   platform: process.platform
 });
